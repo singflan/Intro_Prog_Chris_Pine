183525ec09e17579588ecbfcfcaa1b1efd8f462c
Dustin Flanary <singflan@gmail.com>
2017-01-25 13:39:06 -0700

    Changed name of file try_Ch.7.rb to 99_bottles_of_beer

---
+++ 99_bottles_of_beer.rb
@@ -0,0 +1,19 @@
+# 99 Bottles of Beer on the Wall
+num_bottles = 99
+word = "bottles"
+while num_bottles > 0
+
+  puts "#{num_bottles} #{word} of beer on the wall,\t#{num_bottles} #{word} of beer."
+  
+  num_bottles -= 1
+  
+  if num_bottles == 1
+    word = "bottle"
+  elsif num_bottles == 0
+    word = "bottles"
+  end
+    
+  puts "Take 1 down, pass it around,\t#{num_bottles} #{word} of beer on the wall."
+end
+
+#Deaf Grandma
